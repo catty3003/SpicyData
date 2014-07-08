13f364c0d64a83a09df947b343a2dac146232f98
Mai <catty_chan16@yahoo.com>
2014-07-09 00:24:26 +0200

    Reference mit author jahr und titel

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -8,4 +8,8 @@ class Reference < ActiveRecord::Base
 	has_many :tenacities
 	has_many :contaminationstatuses
 	belongs_to :adminreferencetype 
+
+	def full_ref
+    self.id.to_s + ". " + self.first_author_name + " (" + self.year.to_s + "): " + self.title
+  end
 end
